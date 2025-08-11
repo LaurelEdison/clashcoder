714f9f05dea05c5cc6a97c20f8a53758108d5c3f
EdisonLaurel <arel.ferdinand@gmail.com>
2025-08-11 23:42:54 +0700

    refactor(backend): Removed template snippets from main

--- backend/main.go
+++ backend/main.go
@@ -8,12 +8,6 @@ import (
 	"github.com/joho/godotenv"
 )
 
-func greet(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World! %s", time.Now())
-}
-
 func main() {
 	godotenv.Load(".env")
-	http.HandleFunc("/", greet)
-	http.ListenAndServe(":8080", nil)
 }
