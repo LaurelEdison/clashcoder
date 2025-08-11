4e3b65190da58826cfd400379a369d686517314e
EdisonLaurel <arel.ferdinand@gmail.com>
2025-08-12 00:00:36 +0700

    feat(backend): Added zap logger, improved port loading with env check

--- backend/main.go
+++ backend/main.go
@@ -3,11 +3,28 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/joho/godotenv"
+	"go.uber.org/zap"
 )
+
+func getPort(zapLogger *zap.Logger) string {
+	portstring := os.Getenv("PORT")
+	if portstring == "" {
+		zapLogger.Warn("PORT not set, defaulting to 8080")
+		return "8080"
+	}
+	zapLogger.Info("PORT loaded from environment", zap.String("port", portstring))
+	return portstring
+}
 
 func main() {
 	godotenv.Load(".env")
+	zapLogger, _ := zap.NewProduction()
+	defer zapLogger.Sync()
+
+	portstring := getPort(zapLogger)
+	zapLogger.Info("Starting server ", zap.String("port", portstring))
 }
