0520b94b181dd0e16b861b7373cd65c0644c57f3
EdisonLaurel <arel.ferdinand@gmail.com>
2025-08-15 20:15:33 +0700

    feat(db): Add users table migration

---
+++ backend/db/schema/001_users.sql
@@ -0,0 +1,16 @@
+-- +goose Up
+
+CREATE TABLE users (
+	id UUID PRIMARY KEY,
+	created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+	updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
+	name TEXT NOT NULL,	
+	email TEXT UNIQUE NOT NULL,
+	password_hash TEXT NOT NULL,
+	last_login_at TIMESTAMP,
+	CONSTRAINT email_format CHECK (email ~* '^[^@]+@[^@]+\.[^@]+$')
+);
+
+-- +goose Down
+
+DROP TABLE users;
