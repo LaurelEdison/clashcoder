bf7c2eb511ba2d683adaa238b9a64c86de7985ac
EdisonLaurel <arel.ferdinand@gmail.com>
2025-08-17 12:53:00 +0700

    feat(auth): Implement auth package for jwt authentication
    - Added helper function to getJWTSecret
    - Added function to generateJWT
    - Added a login function that checks password hash, generate jwt, and
      returns it
    - Added jwt middleware checks for Authorization: Bearer <token>
    - Added key type for userid key context

---
+++ backend/handlers/auth/auth.go
@@ -0,0 +1,120 @@
+package auth
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/LaurelEdison/clashcoder/backend/handlers"
+	"github.com/golang-jwt/jwt"
+	"github.com/google/uuid"
+)
+
+func getJWTSecret() []byte {
+	secret := os.Getenv("JWT_SECRET")
+	if secret == "" {
+		panic("JWT_SECRET is not set")
+	}
+	return []byte(secret)
+}
+
+var jwtSecret = getJWTSecret()
+
+func generateJWT(userID uuid.UUID) (string, error) {
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"user_id": userID.String(),
+		"exp":     time.Now().Add(time.Hour * 72).Unix(),
+	})
+
+	return token.SignedString(jwtSecret)
+
+}
+
+func Login(h *handlers.Handlers) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		type parameters struct {
+			Email    string `json:"email"`
+			Password string `json:"password"`
+		}
+
+		params := parameters{}
+
+		if err := json.NewDecoder(r.Body).Decode(&params); err != nil {
+			h.RespondWithError(w, http.StatusBadRequest, "Invalid JSON")
+			return
+		}
+
+		user, err := h.DB.GetUserByEmail(r.Context(), params.Email)
+		if err != nil {
+			h.RespondWithError(w, http.StatusUnauthorized, "Invalid email or password")
+			return
+		}
+		if err := handlers.CheckPasswordHash(user.PasswordHash, params.Password); !err {
+			h.RespondWithError(w, http.StatusUnauthorized, "Invalid email or password")
+			return
+		}
+
+		token, err := generateJWT(user.ID)
+		if err != nil {
+			h.RespondWithError(w, http.StatusInternalServerError, "Could not generate token")
+			return
+		}
+
+		h.RespondWithJSON(w, 200, map[string]string{"token": token})
+
+	}
+}
+
+type contextKey string
+
+const (
+	userIDKey contextKey = "user_id"
+)
+
+func JWTAuthMiddleWare(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		authHeader := r.Header.Get("Authorization")
+		if authHeader == "" || !strings.HasPrefix(authHeader, "Bearer ") {
+			http.Error(w, "Missing or invalid token", http.StatusUnauthorized)
+			return
+		}
+
+		tokenString := strings.TrimPrefix(authHeader, "Bearer ")
+		token, err := jwt.Parse(tokenString, func(t *jwt.Token) (interface{}, error) {
+			if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, fmt.Errorf("unexpected signing method: %v", t.Header["Alg"])
+			}
+			return jwtSecret, nil
+		})
+
+		if err != nil || !token.Valid {
+			http.Error(w, "Invalid token", http.StatusUnauthorized)
+			return
+		}
+
+		if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+			uidStr, ok := claims["user_id"].(string)
+			if !ok {
+				http.Error(w, "Invalid token", http.StatusUnauthorized)
+				return
+			}
+
+			userID, err := uuid.Parse(uidStr)
+			if err != nil {
+				http.Error(w, "Invalid token", http.StatusUnauthorized)
+				return
+			}
+			ctx := context.WithValue(r.Context(), userIDKey, userID)
+			next.ServeHTTP(w, r.WithContext(ctx))
+		} else {
+			http.Error(w, "Invalid token", http.StatusUnauthorized)
+			return
+		}
+
+	})
+}
