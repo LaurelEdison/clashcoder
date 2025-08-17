f496d848fc258b8e3222e507bf7ea7980b0996ca
EdisonLaurel <arel.ferdinand@gmail.com>
2025-08-17 13:08:54 +0700

    feat(routes): Added route for logged in user to fetch profile

--- backend/routes/routes.go
+++ backend/routes/routes.go
@@ -2,10 +2,12 @@ package routes
 
 import (
 	"github.com/LaurelEdison/clashcoder/backend/handlers"
+	"github.com/LaurelEdison/clashcoder/backend/handlers/auth"
 	"github.com/go-chi/chi/v5"
 )
 
 func SetupRoutes(router chi.Router, h *handlers.Handlers) {
 	router.Get("/healthz", h.HandlerHealth)
 	router.Post("/users", h.SignUp)
+	router.With(auth.JWTAuthMiddleWare).Get("/me", h.FetchProfileSelf)
 }
