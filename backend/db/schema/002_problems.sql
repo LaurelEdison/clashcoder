3ec9bf0417cf13ac1610917e8a855d611e9c6ea1
EdisonLaurel <arel.ferdinand@gmail.com>
2025-08-18 19:49:18 +0700

    feat(db): Add problems postgres migration

---
+++ backend/db/schema/002_problems.sql
@@ -0,0 +1,16 @@
+-- +goose Up
+
+CREATE TABLE problems (
+	id UUID PRIMARY KEY,
+	created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+	updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
+	title TEXT NOT NULL,
+	description TEXT NOT NULL,
+	difficulty TEXT check (difficulty in ('easy', 'medium', 'hard')),
+	time_limit INT NOT NULL DEFAULT 2000,
+	memory_limit_mb INT NOT NULL DEFAULT 256
+);
+
+-- +goose Down
+
+DROP TABLE problems;
