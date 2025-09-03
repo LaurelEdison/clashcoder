824b98ff5094d259f277e10255a59e02c14294cd
EdisonLaurel <arel.ferdinand@gmail.com>
2025-09-03 19:57:43 +0700

    feat(lobby): Add queries to create, get, remove lobby users
    - Add separate getter for host and user

--- backend/db/queries/lobby_users.sql
+++ backend/db/queries/lobby_users.sql
@@ -1,1 +1,13 @@
+-- name: CreateLobbyUser :one
+INSERT INTO lobby_users(lobby_id, user_id, joined_at, role)
+VALUES($1,$2,$3,$4)
+RETURNING *;
 
+-- name: GetLobbyUsersByLobbyID :many
+SELECT * FROM lobby_users WHERE lobby_id = $1;
+
+-- name: GetHostFromLobbyID :one
+SELECT * FROM lobby_users WHERE lobby_id = $1 AND role = 'host';
+
+-- name: RemoveLobbyUserFromLobby :exec
+DELETE FROM lobby_users WHERE lobby_id = $1 AND user_id = $2;
