01961223426f8e6939f53343fecc77ea41251988
EdisonLaurel <arel.ferdinand@gmail.com>
2025-09-03 20:14:19 +0700

    feat(lobby): Add function for users to join a lobby

--- backend/handlers/lobby/lobby_user.go
+++ backend/handlers/lobby/lobby_user.go
@@ -11,3 +11,32 @@ import (
 	"github.com/google/uuid"
 )
 
+func CreateLobbyUser(h *handlers.Handlers) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		LobbyIDstr := chi.URLParam(r, "lobby_id")
+		LobbyID, err := uuid.Parse(LobbyIDstr)
+		if err != nil {
+			h.RespondWithError(w, http.StatusBadRequest, "Failed decoding lobby id")
+			return
+		}
+		UserID, ok := users.GetUserId(r.Context())
+		if !ok {
+			h.RespondWithError(w, http.StatusUnauthorized, "Failed getting userid")
+			return
+		}
+
+		LobbyUser, err := h.DB.CreateLobbyUser(r.Context(), database.CreateLobbyUserParams{
+			LobbyID:  LobbyID,
+			UserID:   UserID,
+			JoinedAt: time.Now(),
+			Role:     "player",
+		})
+		if err != nil {
+			h.RespondWithError(w, http.StatusInternalServerError, "Failed to create lobby user")
+			return
+		}
+		h.RespondWithJSON(w, http.StatusOK, handlers.DatabaseLobbyUserToLobbyUser(LobbyUser))
+
+	}
+}
+
